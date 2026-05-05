96bb06833890bc6f35436b82a2a15b3911e8aeb4
YourName <your@email.com>
2026-05-05 12:47:14 +0400

    v1: book manager CLI app with CRUD and JSON

---
+++ book.py
@@ -0,0 +1,97 @@
+import json
+
+filename="books.json"
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title=title
+        self.author=author
+        self.year=year
+    def introduce(self):
+        print(f"Title: {self.title} | Author: {self.author} | Year: {self.year}")
+    
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "year": self.year
+        }
+    
+class BookManager:
+    def __init__(self):
+        self.books=[]
+    
+    def add_book(self, book):
+        self.books.append(book)
+        print("Added successfuly")
+    
+    def show_books(self):
+        print("\n---ALL BOOKS---")
+        if not self.books:
+            print("no books yet")
+            return
+        for book in self.books:
+            book.introduce()
+    
+    def search_book(self, title):
+        print(f"searching: {title}")
+        for book in self.books:
+         if title.lower() in book.title.lower():
+            book.introduce()
+            return
+    print("not found")
+
+    def delete_book(self, title):
+        print(f"delete: {title}")
+        for book in self.books:
+         if title.lower() in book.title.lower():
+            self.books.remove(book)
+            print("deleted successfully")
+            return
+    print("not found")
+
+    def update_book(self, title):
+        for book in self.books:
+          if title.lower() in book.title.lower():
+            new_title = input("title: ")
+            new_author = input("author: ")
+            new_year = int(input("year: "))
+
+            book.title = new_title
+            book.author = new_author
+            book.year = new_year
+
+            print("updated successfully")
+            return
+    print("book not found")
+
+    def count_by_author(self, author):
+        count = 0
+        for book in self.books:
+            if author.lower() in book.author.lower():
+                count += 1
+
+        if count > 0:
+          print(f"{count} books found for {author}")
+        else:
+           print("no book from this author")
+    
+    def save_to_file(self):
+       data=[]
+       for book in self.books:
+           data.append(book.to_dict())
+       with open(filename, "w") as file:
+          json.dump(data, file)
+    
+    def load_from_file(self):
+       try:
+          with open(filename, "r") as file:
+             data = json.load(file)
+             self.books = []
+             for item in data:
+                 book = Book(item["title"], item["author"], item["year"])
+                 self.books.append(book)
+       except FileNotFoundError:
+           print("no file yet")
+       except json.JSONDecodeError:
+           print("failed to load books")
