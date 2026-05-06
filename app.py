57bc3d5cddc461c00763fd93c3c8753b15fdf71c
YourName <your@email.com>
2026-05-06 15:39:50 +0400

    fix render

--- app.py
+++ app.py
@@ -102,5 +102,8 @@ def search():
 
 
 # 🚀 RUN
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
